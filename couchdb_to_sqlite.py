c77a0af12627e51cdd6b14e774d30cc16a5b1230
Marie-Luce <marie-luce@llee.eu>
2016-01-04 00:26:43 +0100

    Gestion des cas ou bank, subcategory sont null

--- couchdb_to_sqlite.py
+++ couchdb_to_sqlite.py
@@ -4,6 +4,9 @@
 from couchdbkit import *
 import sqlite3
 
+sqlite_file = 'db.sqlite3.account'
+bank = 'soge'
+
 # server object
 server = Server()
 
@@ -11,7 +14,7 @@ server = Server()
 db = server.create_db('account')
 
 # connexion a la base sqlite
-conn = sqlite3.connect('db.sqlite3.account')
+conn = sqlite3.connect(sqlite_file)
 cursor = conn.cursor()
 
 # on recupere tous les doc
@@ -29,11 +32,25 @@ for d in all_docs :
 
     # on recupere un document
     doc = db.get(id_doc)
+
     if doc['category'] == None: 
+        print "Aucune categorie trouvé pour ce document :"
+        print doc
         doc['category'] = 'autre' 
-        doc['subcategory'] = 'autre' 
-    if doc['comment'] == None: 
-        doc['comment'] = "" 
+        doc['subcategory'] = 'autre'
+        print "Document migré avec 'category' et 'subcategory' égalent à 'autre'"
+        print ""
+    if doc['bank'] == None: 
+        print "Aucune banque trouvée pour ce document :"
+        print doc
+        doc['bank'] = bank 
+        print "Document migré avec 'bank' égale à " + bank 
+        print ""
+    try:
+        if doc['comment'] == None: 
+            doc['comment'] = "" 
+    except KeyError:
+            doc['comment'] = "" 
 
     data = (i, doc['date'], doc['description'], doc['expense'], doc['category'], doc['bank'], doc['check'], doc['halve'], doc['comment'], doc['subcategory'])
 
