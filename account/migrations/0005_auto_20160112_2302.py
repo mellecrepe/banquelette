e8b1d57eb94b2b8c9bc685e30600353f5c1ca6e8
Marie-Luce <marie-luce@llee.eu>
2016-01-27 23:32:56 +0100

    Migration base de données

---
+++ account/migrations/0005_auto_20160112_2302.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('account', '0004_auto_20151227_2135'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='account',
+            name='bank',
+            field=models.CharField(max_length=50, choices=[(None, b''), (b'oney', b'Oney'), (b'boursorama', b'Boursorama'), (b'soge', b'Soci\xc3\xa9t\xc3\xa9 G\xc3\xa9n\xc3\xa9rale'), (b'ingdirect', b'ING Direct'), (b'espece', b'Liquide/Esp\xc3\xa8ce')]),
+        ),
+    ]
