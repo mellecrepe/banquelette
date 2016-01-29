c807a2aa3a4f0da7233a0d8a5697cce60d56c939
Dok Slade <dok.slade@gmail.com>
2016-01-29 13:11:50 +0100

    Supression des warnings Django 1.10

--- account/urls.py
+++ account/urls.py
@@ -3,16 +3,17 @@ from django.views.generic import ListView
 from account.models import Account
 from django.views.generic import RedirectView
 from django.core.urlresolvers import reverse_lazy
+from . import views
 
-urlpatterns = patterns('account.views',
-    url(r'^accueil$', 'home'),
-    url(r'^release$', 'release'),
-    url(r'^add$', 'db_add'),
-    url(r'^update$', 'db_update'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})$', 'month_view'),
-    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<category>\w+)$', 'month_view'),
-    url(r'^modify/(?P<year>\d{4})/(?P<month>\d{2})$', 'db_modify'),
-    url(r'^modify/nocheck$', 'db_modify'),
-    url(r'^modify/bymonth$', 'month_choice'),
+urlpatterns = [
+    url(r'^accueil$', views.home),
+    url(r'^release$', views.release),
+    url(r'^add$', views.db_add),
+    url(r'^update$', views.db_update),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})$', views.month_view),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<category>\w+)$', views.month_view),
+    url(r'^modify/(?P<year>\d{4})/(?P<month>\d{2})$', views.db_modify),
+    url(r'^modify/nocheck$', views.db_modify),
+    url(r'^modify/bymonth$', views.month_choice),
     url(r'^$', RedirectView.as_view(url='accueil'), name='redirect_home'),
-)
+]
